156e7c729ef3a25e9f737a0080048979574cb74c
ishwaryachitsen0523 <ishwaryachitsen98@gmail.com>
2017-06-28 14:24:57 +0530

    Update ish.c

--- ish.c
+++ ish.c
@@ -1,1 +1,24 @@
-nrhigt6dkuh
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("enter the number:");
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("the number is positive");
+  }
+  else if(a<0)
+  {
+    printf("the number is negative");
+  }
+  else
+  {
+    printf("the number is zero");
+  }
+  getch();
+}
+  
